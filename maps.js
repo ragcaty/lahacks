aa4d6a5e44ee197eefed86449a0391a1e32e7c79
Matth792 <Matth792@yahoo.com>
2015-04-04 16:52:06 -0700

    Clickable info boxes first test

--- maps.js
+++ maps.js
@@ -2,6 +2,7 @@ var directionsDisplay;
 var directionsService = new google.maps.DirectionsService();
 var map;
 
+// Where the map starts
 function initialize() {
   directionsDisplay = new google.maps.DirectionsRenderer();
   var chicago = new google.maps.LatLng(41.850033, -87.6500523);
@@ -24,13 +25,30 @@ function calcRoute() {
   directionsService.route(request, function(response, status) {
     if (status == google.maps.DirectionsStatus.OK) {
       var start, end;
+	  
+	  
+	   var contentString = '<div id="content">'+
+      '<p>Test info box</p>' +
+      '</div>';
+	   var infowindow = new google.maps.InfoWindow({
+      content: contentString
+  });
+  
+  
       for(var i = 0; i<response.routes[0].overview_path.length; i++) {
         end = response.routes[0].overview_path[i];
         if(i == 0 || haversine(start, end) > 5) {
           var marker = new google.maps.Marker({
             position: response.routes[0].overview_path[i],
             map: map
+			title: 'Test Title'
           });
+		  
+		   google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map,marker);
+  });
+  
+  
           start = response.routes[0].overview_path[i];
         }
       }
@@ -38,9 +56,11 @@ function calcRoute() {
     }
   });
 }
+
 Number.prototype.toRadians = function() {
   return this * Math.PI / 180;
 }
+
 function haversine(end, start) {
   var dLon = end.lng().toRadians()-start.lng().toRadians();
   var dLat = end.lat().toRadians()-start.lat().toRadians();
