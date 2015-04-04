88b501d3dd3157a760a07c403c8136bbb83364ab
Sarah Wong1 <sarahwong1@Sarahs-MacBook-Pro.local>
2015-04-04 14:24:20 -0700

    Markers spawn every 5 miles

--- maps.js
+++ maps.js
@@ -6,8 +6,8 @@ function initialize() {
   directionsDisplay = new google.maps.DirectionsRenderer();
   var chicago = new google.maps.LatLng(41.850033, -87.6500523);
   var mapOptions = {
-    zoom:7,
-    center: chicago
+    center: { lat: 34.067728, lng: -118.447223},
+    zoom: 15
   };
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
   directionsDisplay.setMap(map);
@@ -23,9 +23,31 @@ function calcRoute() {
   };
   directionsService.route(request, function(response, status) {
     if (status == google.maps.DirectionsStatus.OK) {
+      var start, end;
+      for(var i = 0; i<response.routes[0].overview_path.length; i++) {
+        end = response.routes[0].overview_path[i];
+        if(i == 0 || haversine(start, end) > 5) {
+          var marker = new google.maps.Marker({
+            position: response.routes[0].overview_path[i],
+            map: map
+          });
+          start = response.routes[0].overview_path[i];
+        }
+      }
       directionsDisplay.setDirections(response);
     }
   });
+}
+Number.prototype.toRadians = function() {
+  return this * Math.PI / 180;
+}
+function haversine(end, start) {
+  var dLon = end.lng().toRadians()-start.lng().toRadians();
+  var dLat = end.lat().toRadians()-start.lat().toRadians();
+  var a = (Math.pow(Math.sin(dLat/2), 2)) + Math.cos(start.lat().toRadians()) * Math.cos(end.lat().toRadians()) * Math.pow(Math.sin(dLon/2), 2);
+  var c = 2*Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var d = 3961 * c;
+  return d;
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
